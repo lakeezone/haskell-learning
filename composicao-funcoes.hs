49865b4fa3ad5523838aef0f0462f244076ea688
Lake <146397816+lakeezone@users.noreply.github.com>
2025-03-15 23:20:10 -0300

    Delete composicao-funcoes.hs

--- composicao-funcoes.hs
+++
@@ -1,8 +0,0 @@
-operacaoEncadeada :: Int -> Int -> Int -> Maybe Int
-operacaoEncadeada x y z = divisaoSegura x y >>= \resultado1 ->
-                          divisaoSegura resultado1 z
-
-main :: IO ()
-main = do
-    print (operacaoEncadeada 20 2 2)  -- Saída: Just 5 (20 / 2 = 10, 10 / 2 = 5)
-    print (operacaoEncadeada 20 0 2)  -- Saída: Nothing
